16028247d7f4ec18389d0f6b902a0e381af22fff
James <apisitamornktn@gmail.com>
2026-02-06 10:03:09 +0700

    fix/path

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { EmployeeComponent } from './pages/employee/employee';
-import { Employeelist } from './pages/employeelist/employeelist';
+import { Home } from '../pages/home/home';
+import { EmployeeComponent } from '../pages/employee/employee';
+import { Employeelist } from '../pages/employeelist/employeelist';
 
 export const routes: Routes = [
   { path: '', component: Home },
